6d73a679b78120f86f8c436ffaa726b78a75c1a6
Paul McBroom <paul.mcbroom.0925@students.mwsu.edu>
2013-02-23 17:06:52 -0600

    changed to perl

---
+++ second.pl
@@ -0,0 +1,27 @@
+!usr/bin/perl
+
+print "<html>"; 
+
+print "<head>"; 
+print "<title> Paul's Page </title>";
+print "</head>";
+
+print "<body bgcolor="white" text="blue">";
+
+print "<h1> Paul's Page</h1>";
+
+print "<a href ="index.html">Home page</a>print <br>";
+
+print "I can count to 10!<br>";
+
+
+
+for(int $i=1; $i<=10; $i++)
+{
+	print "$i, ah ah ah! <br>\n";
+}
+?>
+
+print "</body>";
+
+print "</html>";
