0257d251d421fbd38348ea4e41962002809e2580
Paul McBroom <paul.mcbroom.0925@students.mwsu.edu>
2013-02-23 17:46:55 -0600

    try again

--- second.pl
+++ second.pl
@@ -1,4 +1,4 @@
-#!usr/bin/perl
+#!D:\Perl\bin\perl
 
 print "<html>"; 
 
@@ -6,21 +6,21 @@ print "<head>";
 print "<title> Paul's Page </title>";
 print "</head>";
 
-print "<body bgcolor="white" text="blue">";
+print "<body bgcolor=\"white\" text=\"blue\">";
 
 print "<h1> Paul's Page</h1>";
 
-print "<a href ="index.html">Home page</a>print <br>";
+print "<a href =\"index.html\">Home page</a>print <br>";
 
 print "I can count to 10!<br>";
 
 
 
-for(int $i=1; $i<=10; $i++)
+for($i=1; $i<=10; $i++)
 {
-	print "$i, ah ah ah! <br>\n";
+	print $i.", ah ah ah! <br>\n";
 }
-?>
+
 
 print "</body>";
 
